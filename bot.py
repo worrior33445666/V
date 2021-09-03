447b3a45aee1f8c64231daf8bdd9fc2055eb15bf
Sohag <sohagjabed10@gmail.com>
2021-09-03 12:33:03 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,76 @@
+from pornhub_api import PornhubApi
+from pornhub_api.backends.aiohttp import AioHttpBackend
+from pornhub_api.modules import video
+from pyrogram import Client, filters
+from pyrogram.types import (InlineQuery, InlineQueryResultArticle,
+                            InputTextMessageContent, Message)
+import youtube_dl
+import os
+
+from config import Config
+import wget
+
+app = Client("pornhub_bot",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN)
+
+if os.path.exists("downloads"):
+    print("Download Path Exist")
+else:
+    print("Download Path Created")
+
+
+@app.on_inline_query()
+async def search(client, InlineQuery : InlineQuery):
+    query = InlineQuery.query
+    backend = AioHttpBackend()
+    api = PornhubApi(backend=backend)
+    src = await api.search.search(query, ordering="mostviewed")
+    videos = src.videos
+    await backend.close()
+    
+
+    results = []
+
+    async for vid in videos:
+        results.append(InlineQueryResultArticle(
+            title=vid.title,
+            input_message_content=InputTextMessageContent(
+                message_text=f"/porn {vid.url}"
+            ),
+            description=f"Duration : {vid.duration}\nViews : {vid.views}\nRating : {vid.rating}",
+            thumb_url=vid.thumb
+        ))
+
+    await InlineQuery.answer(results)
+    
+
+@app.on_message(filters.command("porn"))
+async def download_video(client, message : Message):
+    await message.reply("Downloading...")
+    url = message.command[1]
+    a = url.replace("unknownview_video.php", "")
+    print(url)
+    # path = wget.download(url)
+    # print(path)
+
+    ydl_opts = {
+            'format': 'best',
+            'outtmpl': "downloads",
+            'nooverwrites': True,
+            'no_warnings': False,
+            'ignoreerrors': True,
+        }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    for file in os.listdir("downloads"):
+        await message.reply_video(f"downloads/{file}", caption="Here Is your Requested Video")
+
+
+
+
+
+app.run()
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+
+    API_ID = 1927002
+    API_HASH = "a5b3aa12e55b8183353a1039ccd109e1"
+    BOT_TOKEN = "1985378806:AAHJ49L62MyVaNqH2xf1-17RzTuepl1vDwE"
