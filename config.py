57d75358268b5cf2e9d3b6f782f57c740eff2e7d
Sohag <sohagjabed10@gmail.com>
2021-09-05 16:18:13 +0530

    Production

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config:
     API_ID = 1927002
     API_HASH = "a5b3aa12e55b8183353a1039ccd109e1"
     BOT_TOKEN = "1985378806:AAHJ49L62MyVaNqH2xf1-17RzTuepl1vDwE"
+    MONGO = "mongodb+srv://pornhubbot:pornhubbot@cluster0.3grqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
